47938c083eaeaf71910f3de98def52a3b24ecaa4
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-07 12:52:41 +0000

    figured out how to test errors

--- test.rb
+++ test.rb
@@ -3,4 +3,13 @@ describe Fixnum do
   it 'returns the sum of its arguments' do
     expect(1.+2).to eq(3)
   end
+  it "reports an error" do
+    expect{2/0}.to raise_error(ZeroDivisionError,'divided by 0')
+  end
+end
+
+describe Jobs do
+  it "testing arrays in Ruby" do
+    expect(['a','b','c']).to eq(['a','b','c'])
+  end
 end
