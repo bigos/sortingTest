96ad35def6820dda225a527de91c58e596e21754
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-07 14:03:00 +0000

    proper handling of  last error

--- jobs.rb
+++ jobs.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'tsort'
 
 
@@ -6,6 +7,17 @@ class Hash
   alias tsort_each_node each_key
   def tsort_each_child(node, &block)
     fetch(node).each(&block)
+  end
+end
+
+class Jobs < Hash
+  def sort(structure)
+    puts 'going to sort'
+    begin
+      puts structure.tsort.inspect
+    rescue Cyclic
+      raise 'jobs can’t have circular dependencies'
+    end
   end
 end
 
@@ -22,11 +34,21 @@ p ({ :a =>[],
      :e =>[:b],
      :f =>[]}.tsort)
 
-p ({:a=>[],:b=>[],:c=>[:c]}.tsort)
+# p ({:a=>[],:b=>[],:c=>[:c]}.tsort)
 
-p ({ :a =>[],
-     :b =>[:c],
-     :c =>[:f],
-     :d =>[:a],
-     :e =>[],
-     :f =>[:b]}.tsort)
+# p ({ :a =>[],
+#      :b =>[:c],
+#      :c =>[:f],
+#      :d =>[:a],
+#      :e =>[],
+#      :f =>[:b]}.tsort)
+
+
+#############
+struct = { :a =>[],
+  :b =>[:c],
+  :c =>[:f],
+  :d =>[:a],
+  :e =>[],
+  :f =>[:b]}
+Jobs.new.sort struct
